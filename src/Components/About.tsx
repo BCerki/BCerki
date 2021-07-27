239b9b742966e52e50c8177fd853bebdca617bc3
unknown <briannac@shaw.ca>
2021-07-27 03:38:13 +0100

    add blurb to about page

--- src/Components/About.tsx
+++ src/Components/About.tsx
@@ -3,8 +3,13 @@ const About: React.FC = () => {
     <div>
       <h1>About</h1>
       <div className="image-container">
-        <img className="banner-image" src="./Banner-high res-3.jpg" />
+        <img className="banner-image" src="Images/Banner-high res-3.jpg" />
       </div>
+      <p>
+        Welcome! My name is Brianna and I live in British Columbia, Canada. I'm
+        the author of two hi-lo novels, <em>Avalanche</em> and{" "}
+        <em>Summer Camp</em>.
+      </p>
     </div>
   );
 };
