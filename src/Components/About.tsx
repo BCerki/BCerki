9a6955cf1d750410d153a99277e4feadde87f6ef
unknown <briannac@shaw.ca>
2021-07-27 03:02:24 +0100

    create about, books, and contact components

---
+++ src/Components/About.tsx
@@ -0,0 +1,11 @@
+const About: React.FC = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <div className="image-container">
+        <img className="banner-image" src="./Banner-high res-3.jpg" />
+      </div>
+    </div>
+  );
+};
+export default About;
---
+++ src/Components/Books.tsx
@@ -0,0 +1,8 @@
+const Books: React.FC = () => {
+  return (
+    <div>
+      <h1>Books</h1>
+    </div>
+  );
+};
+export default Books;
