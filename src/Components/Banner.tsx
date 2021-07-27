19dc9b418f99ff0a4f529ff630ab9ea7c70af0b2
unknown <briannac@shaw.ca>
2021-07-27 03:03:47 +0100

    add image and styling classes to banner component

--- src/Components/Banner.tsx
+++ src/Components/Banner.tsx
@@ -1,4 +1,14 @@
 const Banner: React.FC = () => {
-  return <div>I'm a banner</div>;
+  return (
+    <div className="image-container">
+      <img className="banner-image" src="./IMG_1024 Copy.jpg" />
+      <div className="centered">
+        <div>
+          <span className="brianna">Brianna Cerkiewicz</span>
+        </div>
+        <div className="author">Author</div>
+      </div>
+    </div>
+  );
 };
 export default Banner;
