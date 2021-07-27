4dc5f06ae237f4e242e82535d7378c1e114fd244
unknown <briannac@shaw.ca>
2021-07-27 03:38:30 +0100

    update image src path

--- src/Components/Banner.tsx
+++ src/Components/Banner.tsx
@@ -1,7 +1,7 @@
 const Banner: React.FC = () => {
   return (
     <div className="image-container">
-      <img className="banner-image" src="./IMG_1024 Copy.jpg" />
+      <img className="banner-image" src="Images/IMG_1024 Copy.jpg" />
       <div className="centered">
         <div>
           <span className="brianna">Brianna Cerkiewicz</span>
