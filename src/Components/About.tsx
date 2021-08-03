04d14314b1262e4b5e1b0604a3097cff0f4fdd89
unknown <briannac@shaw.ca>
2021-08-03 22:46:11 +0100

    play with dropshadow box for text

--- src/Components/About.tsx
+++ src/Components/About.tsx
@@ -6,11 +6,13 @@ const About: React.FC = () => {
       {/* <div className="image-container">
         <img className="banner-image" src="Images/Banner-high res-3.jpg" />
       </div> */}
-      <p>
-        Welcome! My name is Brianna and I live in British Columbia, Canada. I'm
-        the author of two hi-lo novels, <em>Avalanche</em> and{" "}
-        <em>Summer Camp</em>.
-      </p>
+      <div className="text">
+        <div className="copy">
+          Welcome! My name is Brianna and I live in British Columbia, Canada.
+          I'm the author of two hi-lo novels, <em>Avalanche</em> and{" "}
+          <em>Summer Camp</em>.
+        </div>
+      </div>
     </div>
   );
 };
